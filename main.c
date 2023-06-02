f260b4dc34ed65ef74cd669f219bef3fd0c3c490
Aaron-Speedy <aaronspeedy@protonmail.com>
2023-06-02 12:15:02 -0500

    Fixed segmentation fault

--- main.c
+++ main.c
@@ -14,16 +14,26 @@ typedef struct {
 } String;
 
 int main() {
+start:
   while(1) {
     char *input = readline("$ ");
-    char *cmd_argv[1] = {strtok(input,  " ")};
+    char *cmd_argv[ARG_BUF_SIZE] = {strtok(input, " ")}; // This should probably become a dynamic array
+    int cmd_argc = 1;
+    
+    // Get arguments
+    int arg_buf_size = ARG_BUF_SIZE;
+    while((cmd_argv[cmd_argc] = strtok(NULL, " ")) != NULL) {
+      cmd_argc++;
+      if(cmd_argc >= arg_buf_size) {fprintf(stderr, "Error: More than 64 arguments\n"); goto start;}
+    }
+    
     if(!strcmp(cmd_argv[0], "cd")) {
-      setenv("PWD", "/", 1);
-      chdir("/");
+      setenv("PWD", cmd_argv[1], 1);
+      chdir(cmd_argv[1]);
     }
     else {
       pid_t pid = fork();
-      if(pid == 0) execvp(cmd_argv[], NULL);
+      if(pid == 0) execvp(cmd_argv[0], NULL);
       else if(pid < 0) perror("Fork");
 
       int status;
